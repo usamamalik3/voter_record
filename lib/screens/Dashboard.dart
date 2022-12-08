63f9000e31cbc5f6245da2ff26f434f7555b0bf2
usamamalik32 <usama0834@gmail.com>
2022-12-08 14:19:00 +0500

    Image slide added

--- lib/screens/Dashboard.dart
+++ lib/screens/Dashboard.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_image_slideshow/flutter_image_slideshow.dart';
 import 'package:voterrecord/auth/athentication.dart';
 import 'package:voterrecord/utils/container_menu.dart';
 
@@ -25,12 +26,10 @@ class _DashBoardState extends State<DashBoard> {
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: <Widget>[
-                 
                   Column(
                     mainAxisAlignment: MainAxisAlignment.center,
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: const <Widget>[
-                   
                       SizedBox(height: 10.0),
                       // Text(
                       //    FirebaseAuth.instance.currentUser!.email.toString(),
@@ -51,8 +50,7 @@ class _DashBoardState extends State<DashBoard> {
               title: const Text('ڈیش بورڈ', style: TextStyle(fontSize: 18)),
               onTap: () {
                 // Here you can give your route to navigate
-                Navigator.pushReplacementNamed(context,
-                    '/dashboard');
+                Navigator.pushReplacementNamed(context, '/dashboard');
               },
             ),
             const Divider(height: 3.0),
@@ -62,17 +60,15 @@ class _DashBoardState extends State<DashBoard> {
               onTap: () {
                 // Here you can give your route to navigate
                 Navigator.pushNamed(context, '/partyform');
-               
               },
             ),
             const Divider(height: 3.0),
-          
+
             ListTile(
               leading: const Icon(Icons.logout),
               title: const Text('Log out', style: TextStyle(fontSize: 18)),
               onTap: () {
                 Authentication.signOut(context: context);
-                
               },
             ),
           ],
@@ -99,7 +95,64 @@ class _DashBoardState extends State<DashBoard> {
         padding: const EdgeInsets.only(top: 15.0),
         child: Column(
           children: [
-            ContainerMenu(list: myList, crossAxisSpacing: 16, mainAxisSpacing: 16, crossAxisCount: 2, childAspectRatio: 0.90)
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: ImageSlideshow(
+                /// Width of the [ImageSlideshow].
+                width: double.infinity,
+
+                /// Height of the [ImageSlideshow].
+                height: 200,
+
+                /// The page to show when first creating the [ImageSlideshow].
+                initialPage: 0,
+
+                /// The color to paint the indicator.
+
+                /// The color to paint behind th indicator.
+                indicatorBackgroundColor: Colors.grey,
+
+                /// Called whenever the page in the center of the viewport changes.
+
+                /// Auto scroll interval.
+                /// Do not auto scroll with null or 0.
+                autoPlayInterval: 3000,
+
+                /// Loops back to first slide.
+                isLoop: true,
+
+                /// The widgets to display in the [ImageSlideshow].
+                /// Add the sample image file into the images folder
+                children: [
+                  Image.asset(
+                    'assets/images/logoslide.png',
+                    fit: BoxFit.cover,
+                  ),
+                  Image.asset(
+                    'assets/images/aaa.png',
+                    fit: BoxFit.cover,
+                  ),
+                  Image.asset(
+                    'assets/images/786.jpg',
+                    fit: BoxFit.cover,
+                  ),
+                  Image.asset(
+                    'assets/images/11111.jpg',
+                    fit: BoxFit.cover,
+                  ),
+                  Image.asset(
+                    'assets/images/000001.jpg',
+                    fit: BoxFit.cover,
+                  ),
+                ],
+              ),
+            ),
+            ContainerMenu(
+                list: myList,
+                crossAxisSpacing: 16,
+                mainAxisSpacing: 16,
+                crossAxisCount: 2,
+                childAspectRatio: 0.90)
           ],
         ),
       ),
